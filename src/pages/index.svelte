cc63b534b0bd5866f66fd35da22ad1fd083aac64
vasikaranb <vasikaran.b@nagarro.com>
2022-11-18 20:20:10 +0530

    dynatrace

--- src/pages/index.svelte
+++ src/pages/index.svelte
@@ -1,10 +1,23 @@
 <script>
-  import { metatags, goto } from "@roxi/routify";
+  import { metatags, goto, afterPageLoad } from "@roxi/routify";
   metatags.title = "My Routify app";
   metatags.description = "Description coming soon...";
+
+  function dyna() {
+    window.dtrum?.setPage({
+			name: 'pathname',
+			group: 'title',
+		});
+  }
+
+  $afterPageLoad(() => { 
+    dyna();
+  });
 </script>
 
 <h1>Routify Starter Template</h1>
+
+
 
 
 
