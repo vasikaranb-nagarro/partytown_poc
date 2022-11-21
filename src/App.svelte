cc6eba305c4dac13de4e9b16837f699c94ce7295
vasikaranb <vasikaran.b@nagarro.com>
2022-11-21 12:08:38 +0530

    glaze issue with base href

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,18 @@
 <script>
   import { Router } from "@roxi/routify";
   import { routes } from "../.routify/routes";
+
+  const config = {
+		queryHandler: {
+			parse: (search) => Object.fromEntries(new URLSearchParams(search)),
+			stringify: (params) => '?' + new URLSearchParams(params).toString(),
+		},
+		urlTransform: {
+			apply: url => `/test${url}`, //external URL
+      remove: url => url.replace('/test', ''), //internal URL
+		},
+		useHash: false,
+	};
 </script>
 
-<Router {routes} />
+<Router {routes} {config}  />
